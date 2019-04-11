333fa3e826350be7beebc35e711d41c61e985ad2
LEIDY JURANI ROMERO SANTANA <lj.romero@sis.virtual.uniandes.edu.co>
2019-04-11 07:56:48 -0500

    Dijkstra funciona, revisar enunciado: todos los caminos desde fuentes hasta destinos

--- src/PrimerPunto/Dijkstra.java
+++ src/PrimerPunto/Dijkstra.java
@@ -8,12 +8,26 @@ import Estructuras.ColaDePrioridadOrientadaMinimo;
  * @author Leidy Romero y David Saavedra
  */
 public class Dijkstra {
-
+	/**
+	 * Vertice sucesor del vertice en la posicion i, la construcción de vertices sucesores conforman el camino
+	 */
 	private int[] camino;
+	/**
+	 * Costo de llegar al vertice de la posicion i, es decir cada posicion del arreglo representa un vertice 
+	 * y el valor del arreglo en esa posicion es el costo de llegar a el
+	 */
 	private int[] costo;
+	/**
+	 * Cola de prioridad para sacar el minimo en cada iteracion
+	 */
 	private ColaDePrioridadOrientadaMinimo pq;
-
-	public Dijkstra(int[][] grafo, int pNumeroVertices, int fuente)
+	/**
+	 * Constructor
+	 * @param matriz matriz que representa al grafo, es una matriz cuadrada, elumero de filas y/o columnas es el numero de vertices
+	 * @param pNumeroVertices numero total de vertices
+	 * @param fuente nodo inicial
+	 */
+	public Dijkstra(int[][] matriz, int pNumeroVertices, int fuente)
 	{
 		camino = new int[pNumeroVertices];
 		costo = new int[pNumeroVertices];
@@ -32,23 +46,43 @@ public class Dijkstra {
 		//se revisa que la cola no este vacia
 		while (!pq.isEmpty()) {
 			int actual = pq.delMin();//elijo el elemento menor de la cola
-			for(int i = 0; i<grafo[actual].length;i++)
+			for(int i = 0; i<matriz[actual].length;i++)
 			{
 				//Revisa si existe un eje entre el vertice actual(fila) y el vertice i (columna) (diferente de -1)
 				//Revisa que el vertice actual no sea el mismo vertice i (diferente de 0)
-				if(grafo[actual][i]>0) 
-					actualizar(grafo, actual, i);//actualiza el costo en la cola 
+				if(matriz[actual][i]>0) 
+					actualizar(matriz, actual, i);//actualiza el costo en la cola 
 			}
 		}
 	}
-
-	private void actualizar(int[][] grafo, int origen, int destino) {
+	/**
+	 * Actualiza el costo en las estructuras auxiliares
+	 * @param matriz matriz que representa al grafo
+	 * @param origen vertice origen
+	 * @param destino vertice destino
+	 */
+	private void actualizar(int[][] matriz, int origen, int destino) {
 		//Si el costo para llegar al destino es mayor que el costo para llegar al origen más el costo "actual", actualizo el costo y el camino
-		if (costo[destino] > costo[origen] + grafo[origen][destino]) {
-			costo[destino] = costo[origen] + grafo[origen][destino];
-			camino[destino] = grafo[origen][destino];
+		if (costo[destino] > costo[origen] + matriz[origen][destino]) {
+			costo[destino] = costo[origen] + matriz[origen][destino];
+			camino[destino] = origen;
 			if (pq.contains(destino)) pq.decreaseV(destino, costo[destino]);
-			else                pq.insert(destino, costo[destino]);
+			else pq.insert(destino, costo[destino]);
 		}
 	}
+	/**
+	 * Retorna el arreglo camino
+	 * @return el arreglo que contiene los sucesores para llegar a todos los vertices
+	 */
+	public int[] getCamino() {
+		return camino;
+	}
+	/**
+	 * Retorna el arreglo costo
+	 * @return el arreglo que contiene los costos minimos para llegar a todos los vertices
+	 */
+	public int[] getCosto() {
+		return costo;
+	}
+	
 }
--- src/PrimerPunto/Main.java
+++ src/PrimerPunto/Main.java
@@ -6,14 +6,14 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-	public final static String PEQUENIO = "";
-	public final static String MEDIO = "";
-	public final static String GRANDE = "";
+	public final static String PEQUENIO = "./data/distances5.txt";
+	public final static String MEDIO = "./data/distances100.txt";
+	public final static String GRANDE = "./data/distances1000.txt";
 
-	private static int[][] grafo;
+	private static int[][] matriz;
+
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		try {
 
 			BufferedReader bf = new BufferedReader(new FileReader(PEQUENIO));
@@ -21,32 +21,49 @@ public class Main {
 			int cont = 0;
 			int j = 0;
 			String[] datos = null;
-			grafo = null;
+			matriz = null;
 			
 			if(linea != null)
 			{
 				datos = linea.split("	");
-				grafo = new int[datos.length][datos.length];
+				matriz = new int[datos.length][datos.length];
 			}
-			
+
 			while(linea != null)
 			{
-				grafo[cont][j] = Integer.parseInt(datos[j]);
-				if(j<datos.length) 
+				matriz[cont][j] = Integer.parseInt(datos[j]);
+				if(j<datos.length-1) 
 				{ j++; }
 				else
 				{
 					linea = bf.readLine();
 					j = 0;
-					datos = linea.split("	");
+					cont++;
+					if(linea!=null)
+						datos = linea.split("	");
 				}
 			}
 		} catch (FileNotFoundException e) {
 			System.err.println("Errores leyendo la matriz");
 		}
-
+		//TODO calcular caminos desde todos los vertices fuente, a todos los vertices destino
+		long tiempoInicial = System.currentTimeMillis();
+		Dijkstra d = new Dijkstra(matriz, matriz[0].length, matriz[0][0]);
+		long tiempoFinal = System.currentTimeMillis();
+		long tiempo  = tiempoFinal-tiempoInicial;
+		int[] camino = d.getCamino();
+		int[] costos = d.getCosto();
 		
-		Dijkstra d = new Dijkstra(grafo,grafo[0].length, grafo[0][0]);
+		System.out.println("Caminos: ");
+		for(int k = 0;k<camino.length;k++)
+		{
+			System.out.println(k+": "+camino[k]);
+		}
+		System.out.println("Costos: ");
+		for(int h = 0;h<costos.length;h++)
+		{
+			System.out.println(h+": "+costos[h]);
+		}
+		System.out.println("Tiempo Dijkstra: "+ tiempo);
 	}
-
 }
